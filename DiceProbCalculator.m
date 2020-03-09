2ee492cb858fb0e51f354c5e4f22204eb1ef5381
Hirad Tabatabaei <54820026+hiradtaba@users.noreply.github.com>
2020-03-09 17:43:52 -0400

    Add files via upload

---
+++ DiceProbCalculator.m
@@ -0,0 +1,51 @@
+prompt1 = ('please enter the number of dice (must be a positive integer): ');
+errorprompt = ('invalid input! please enter a positive integer: ');
+N = input(prompt1);
+while (N<=0)
+    N = input(errorprompt);
+end
+
+prompt2 = ('please enter the number of sides on each dice (must be a positive integer): ');
+Q = input(prompt2);
+while(Q<=0)
+    Q = input(errorprompt);
+end
+
+a = [N:Q*N];
+con = ((Q-1)*N)+1;
+b = zeros(1,con);
+x = Q^N;
+fun = 0;
+syms w
+
+for m = 1:Q
+    fun = fun + w^m;
+end
+
+expfun = expand(fun^N);
+D = coeffs(expfun);
+
+for i = 1:con
+    b(i) = D(i);
+end
+
+prob = b ./ x;
+
+p = max(prob);
+[~,I] = find(prob==p);
+
+Sums = a'; % creating the transpose of the sums array (for the table)
+Frequency = b'; % creating the transpose of the frequency arry (for the table) 
+Probabilities = prob'; % creating the trnaspose of the probabilitites array
+
+output = table(Sums,Frequency,Probabilities) % creating a table containing the sums, the frequencies
+% and the probabilities values
+
+if (length(I)==1)
+   " The sum with the highest probability is: "+a(I)+" " 
+end
+
+if(length(I)==2)
+    " The sums with the highest probability are: "+a(I(1))+" and "+a(I(2))+" "
+end
+
